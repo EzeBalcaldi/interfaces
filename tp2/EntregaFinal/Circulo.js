f7eb90084c7c585f9058d4547a13fe8d5b75c3bf
Ezequiel Balcaldi <ezequielbalcaldi@hotmail.com>
2019-09-09 17:00:19 -0300

    .

--- tp2/EntregaFinal/Circulo.js
+++ tp2/EntregaFinal/Circulo.js
@@ -1,21 +1,51 @@
-class Circulo{
-  constructor (x, y){
-    this.posX = x;
-    this.posY = y;
-  }
+// class Circulo {
+//   constructor (x, y){
+//     this.posX = x;
+//     this.posY = y;
+//   }
+//
+//    dibujar(x, y, canvas, ctx){
+//     ctx.beginPath();
+//     ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
+//     ctx.fillStyle = "rgba(255, 0, 0, 255)";
+//     ctx.fill();
+//     ctx.closePath();
+//   }
+//
 
-   dibujar(x, y, canvas, ctx){
-    ctx.beginPath();
-    ctx.arc(x, y, 5, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "rgba(255, 0, 0, 255)";
-    ctx.fill();
-    ctx.closePath();
-  }
-  
-  getcoordenadaX(){
-    return this.posX;
-  }
-  getcoordenadaY(){
-    return this.posY;
-  }
-  }
+//
+  class Circulo{
+    constructor(x, y, radio, color){
+        this.x=x;
+        this.y=y;
+        this.radio=radio;
+        this.color=color;
+    }
+//MAS ADELANTE
+    cambiarpos(x,y){
+        this.x=x;
+        this.y=y;
+    }
+
+    dibujar(ctx){
+        ctx.beginPath();
+        ctx.strokeStyle = this.color;
+        ctx.arc(this.x, this.y, this.radio, 0, 2 * Math.PI);
+        ctx.stroke();
+    }
+
+    getRadio(){
+      return this.radio;
+    }
+    mover(x,y){
+      this.x += x;
+       this.y += y;
+    }
+
+      getcoordenadaX(){
+        return this.x;
+      }
+      getcoordenadaY(){
+        return this.y;
+      }
+    }
--- tp2/EntregaFinal/Figura.js
+++ tp2/EntregaFinal/Figura.js
@@ -1,7 +1,100 @@
-//class Figura{
-//  let arregloVertices = [];
-//  constructor (arregloVertices){
-//    this.arregloVertices = arregloVertices;
-//  }
+class Figura {
 
-//}
+  constructor(){
+      this.circulos = [];
+      this.centro = null;
+  }
+
+agregarCirculo(circulo){
+  this.circulos.push(circulo);
+}
+
+getCirculos(){
+  return this.circulos;
+}
+
+terminarLinea(){
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  if(this.circulos.length > 1){
+    let circuloOrigen = this.circulos[this.circulos.length -1];
+    let circuloDestino =this.circulos[this.circulos.length -2];
+    this.dibujarLinea(ctx, circuloOrigen, circuloDestino);
+    // if(this.circulos.length >4){
+    //   this.cerrarPoligono();
+    // }
+  }
+}
+
+dibujarLinea(ctx, circuloOrigen, circuloDestino){
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "#FFFF00";
+      ctx.beginPath();
+      ctx.moveTo(circuloDestino.getcoordenadaX(), circuloDestino.getcoordenadaY());
+      ctx.lineTo(circuloOrigen.getcoordenadaX(), circuloOrigen.getcoordenadaY());
+      ctx.stroke();
+}
+
+cerrarPoligono(){
+  console.log("Kmkmkmk");
+  let circuloOrigen = this.circulos[this.circulos.length -1];
+  let circuloDestino =this.circulos[0];
+  this.dibujarLinea(ctx, circuloOrigen, circuloDestino);
+  this.calcularCentro();
+    //adentro de otra funcion del hola
+    // indice ++;
+    // figura[indice] = new Figura();
+  }
+
+
+ calcularCentro(){
+   var canvas = document.getElementById("myCanvas");
+   var ctx = canvas.getContext("2d");
+    let promedioX = 0;
+    let promedioY = 0;
+    for(let i = 0; i< this.circulos.length;i++){
+      promedioX += this.circulos[i].getcoordenadaX();
+      promedioY += this.circulos[i].getcoordenadaY();
+    }
+    promedioX = promedioX / this.circulos.length;
+    promedioY = promedioY / this.circulos.length;
+    let circulo = new Circulo(promedioX, promedioY, 50, "#80ff00")
+    this.centro = circulo;
+    circulo.dibujar(ctx);
+    console.log(promedioX);
+    console.log(promedioY);
+  }
+
+
+
+      clickCentro(e){
+          let d1 = Math.sqrt(Math.pow(e.clientX-this.centro.getcoordenadaX(),2)+Math.pow(e.clientY-this.centro.getcoordenadaY(),2))
+          if (d1<this.centro.getRadio()){
+            console.log("atroden");
+            figura[indice].mover(event, ctx);
+            return true;
+          }else{
+            console.log("afuerovich");
+            return false;
+          }
+        }
+
+mover(e, ctx) {
+    this.centro.dibujar(ctx);
+    for (let i = 0; i < this.circulos.length; i++) {
+        this.circulos[i].mover(e.clientX - this.centro.getcoordenadaX(), e.clientY - this.centro.getcoordenadaY())
+        this.circulos[i].dibujar(ctx);
+        let a = e.clientX - this.centro.getcoordenadaX();
+        let b = e.clientY - this.centro.getcoordenadaY();
+
+        console.log("clientX " + e.clientX);
+        console.log("centro X " + this.centro.getcoordenadaX());
+        console.log("la resta de x es " + a);
+        console.log("clientY " + e.clientY);
+        console.log("centro Y " + this.centro.getcoordenadaY());
+        console.log("la resta de y es " + a);
+    }
+
+
+}
+}
